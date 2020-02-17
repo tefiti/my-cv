3f62283dbfc1dcd9936f3b3c4f5ed73b1fb4448c
tefiti <oanamarinica@yahoo.com>
2020-02-17 14:53:40 +0200

    Updates

---
+++ precache-manifest.f2576eec65e9acbfe7628ffe0b0cd56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9346088e74cd7bcc1b71bab37d1de11e",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "eee6a82e24b6fb1e60a3",
+    "url": "/my-cv/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2bd16ce30ecd3970d0e7",
+    "url": "/my-cv/static/js/2.156266e3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-cv/static/js/2.156266e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee6a82e24b6fb1e60a3",
+    "url": "/my-cv/static/js/main.14efaaa0.chunk.js"
+  },
+  {
+    "revision": "3072575974a33a862144",
+    "url": "/my-cv/static/js/runtime-main.602423d2.js"
+  }
+]);
