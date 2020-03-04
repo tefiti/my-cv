f6cbd2e48b8beb05c7b66d8229120919d5b5602a
tefiti <oanamarinica@yahoo.com>
2020-03-04 16:27:12 +0200

    about me page is ready and responsive

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Slide from '@material-ui/core/Slide';
+import myPicture from '../images/michael-dam-mEZ3PoFGs_k-unsplash.jpg';
+import './about.css';
 
 function About() {
     return (
       <div className="about">
-        <img src='#' alt="woman smiling"></img>
-        <div>
-          <h1>About me</h1>
-          <p>Before the summer of 2019 I had no idea what a developer does, but I gathered my courage to start  some IT courses, 
-          as career tests suggested that it would be a good match for me. I used to be a pharmacist, but I didn't love it and I was 
+      <Grid container>
+      <Grid item className="aboutText" xs={12} md={6}>
+      <Slide direction="right" in={true} timeout={3000} mountOnEnter unmountOnExit>
+        <div className="aboutTextStyle">
+          <h1>about me</h1>
+          <p>I used to be a pharmacist, but I didn't love it and I was 
           searching for something more meaningful.
           </p>
           <p>Now I am determined to become an excellent web developer, even though I sometimes feel overwhelmed of the distance I 
           have to cross to get there.</p>
-          <p>So...why do I love programming? Because:</p>
+          <p>So...why do I love programming? Here are three reasons:</p>
           <p>it challenges me every day with my own limits;</p>
           <p>it allows me to be as creative as a child;</p>
-          <p>I can make things that are useful to other people;</p>
-          <p>after I accomplish a difficult task I feel like a superhero :) </p>
+          <p>I can make things that are useful to other people.</p>
         </div>
-        
+        </Slide>
+        </Grid>
+        <Grid className="myPhoto"item xs={12} md={6}>
+        <Slide direction="left" in={true} timeout={3000} mountOnEnter unmountOnExit>
+          <img className="myPhotoStyle" src={myPicture} alt="woman smiling"></img>
+          </Slide>
+        </Grid>
+      </Grid>  
       </div>
     );
 }
